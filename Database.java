f858d27873a28175ce50338ce55400cca13b81c6
Jimmy <jl5899@rit.edu>
2025-03-18 11:44:03 -0400

    Starting Code

---
+++ Database.java
@@ -0,0 +1,14 @@
+import java.sql.*;
+
+public class Database {
+    public static void main(String[] args) {
+        String url = "jdbc:postgressql://localhost:5432/p32001_22";
+        String username = "";
+        String password = "";
+
+        Connetion con = DriverManager.getConnection(url, username, password);
+        Statement st = con.createStatement();
+        String sql = "";
+        st.executeQuery(sql);
+    }
+}
