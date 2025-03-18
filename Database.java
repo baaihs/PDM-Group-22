c10859963c648f0f10763506dd850e23d4495137
Jimmy <jl5899@rit.edu>
2025-03-18 12:06:47 -0400

    Updated Code

--- Database.java
+++ Database.java
@@ -2,13 +2,17 @@ import java.sql.*;
 
 public class Database {
     public static void main(String[] args) {
-        String url = "jdbc:postgressql://localhost:5432/p32001_22";
+        String url = "jdbc:postgresql://127.0.0.1:5432/p32001_22";
         String username = "";
         String password = "";
 
         Connection con = DriverManager.getConnection(url, username, password);
         Statement st = con.createStatement();
-        String sql = "";
+        String sql = "select * from cast_member";
+        ResultSet rs = st.executeQuery(sql);
         st.executeQuery(sql);
+        rs.next();        
+        String result = rs.getString(sql);
+        System.out.println(result);
     }
 }
