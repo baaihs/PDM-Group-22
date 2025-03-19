80fd9ba25f689287f26ea21c9865cad932a43c29
Ricky Leung <rl9480@rit.edu>
2025-03-19 19:59:54 -0400

    Create mvn project and added database

---
+++ my-project/src/main/java/com/example/Database.java
@@ -0,0 +1,81 @@
+package com.example;
+
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+import java.util.Properties;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.*;
+
+public class Database {
+
+    public static void main(String[] args) throws SQLException {
+
+        int lport = 5432;
+        String rhost = "starbug.cs.rit.edu";
+        int rport = 5432;
+        String user = "YOUR_CS_USERNAME"; //change to your username
+        String password = "YOUR_CS_PASSWORD"; //change to your password
+        String databaseName = "p32001_22"; //change to your database name
+        try (BufferedReader br = new BufferedReader(new FileReader("credentials.txt"))) {
+            user = br.readLine();
+            password = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String driverName = "org.postgresql.Driver";
+        Connection conn = null;
+        Session session = null;
+        try {
+            java.util.Properties config = new java.util.Properties();
+            config.put("StrictHostKeyChecking", "no");
+            JSch jsch = new JSch();
+            session = jsch.getSession(user, rhost, 22);
+            session.setPassword(password);
+            session.setConfig(config);
+            session.setConfig("PreferredAuthentications","publickey,keyboard-interactive,password");
+            session.connect();
+            System.out.println("Connected");
+            int assigned_port = session.setPortForwardingL(lport, "127.0.0.1", rport);
+            System.out.println("Port Forwarded");
+
+            // Assigned port could be different from 5432 but rarely happens
+            String url = "jdbc:postgresql://127.0.0.1:"+ assigned_port + "/" + databaseName;
+System.out.println("database Url: " + url);
+            Properties props = new Properties();
+            props.put("user", user);
+            props.put("password", password);
+
+            Class.forName(driverName);
+            conn = DriverManager.getConnection(url, props);
+            System.out.println("Database connection established");
+
+            // Do something with the database....
+            try (Statement st = conn.createStatement()) {
+                String sql = "SELECT * FROM movie WHERE movie_id = 10001"; // Replace with your actual SQL query
+                try (ResultSet rs = st.executeQuery(sql)) {
+                    while (rs.next()) {
+                        String result = rs.getString(1); // Adjust based on your table structure
+                        System.out.println(result);
+                    }
+                }
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (conn != null && !conn.isClosed()) {
+                System.out.println("Closing Database Connection");
+                conn.close();
+            }
+            if (session != null && session.isConnected()) {
+                System.out.println("Closing SSH Connection");
+                session.disconnect();
+            }
+        }
+    }
+}
